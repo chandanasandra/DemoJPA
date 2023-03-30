5346622d15c6d8de4a8fa6ae73b999dfd0722e2a
chandanasandra <chandanakm273@gmail.com>
2023-03-30 19:25:07 +0530

    First commit

---
+++ demo/src/main/java/com/example/demo/Course.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+		@Id
+		@GeneratedValue
+		private Long id;
+		private String name;
+		
+		protected Course() {
+			
+		}
+		public Course(String name) {
+			this.setName(name);
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Long getId() {
+			return id;
+		}
+		@Override
+		public String toString() {
+			return "Course [name=" + name + "]";
+		}
+		
+}
